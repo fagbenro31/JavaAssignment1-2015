5881f6d92b8ebeb986fc710ed43aab8deb3b421f
fagbenro31 <fagbenro31@gmail.com>
2015-09-14 16:20:19 -0400

    Initial Run

---
+++ src/javaassignment1/pkg2015/JavaAssignment12015.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+ */
+package javaassignment1.pkg2015;
+
+/**
+ *
+ * @author c0647896
+ */
+public class JavaAssignment12015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /****
+         * http://www.recruitingsite.com/csbsites/bestbuy/JobDescription.asp?SuperCategoryCode=12897&JobNumber=754929&JobTitle=JavaDeveloperx2&lang=en&source=indeed.com
+         * JAVA SE (Standard Edition): a widely known used platform for development of portable application for desktop and server environment
+         * J2EE (Enterprise Edition): Provides API & runtime environment for developing & running enterprise software.
+         * PL/SQL (Procedural Language/Structured Query Language) : an extension for SQL and Oracle
+         * XML ( Extensible Markup Language ) : a language that defines a set of rules for encoding documents in a format.
+         * Linux shell scripting : Is a computer program designed to run by UNIX shell, and also a command line interpreter.
+         * Servlets/JSP (Java Servlet) : a program that extend the capabilities of a server
+         * Oracle database 10g/11g : an Object relational db management system.
+         * O/R mapping technology : A technology use for programming technique for converting  data between incomatible type systems in Object Oriented programs.
+         * Web Services : A mode of communication between two electronics device over a network
+         * Testing & Build frameworks: building and testing of frameworks
+         * Spring: An application framework and inversion of control container for java platforms
+         * Toronto Salary (Software Developer I): 60k
+         * Vancouver Salary (Software Developer I):57,985
+         * Waterloo Salary (Software Developer I): 61,755 k
+         */
+        
+    }
+    
+}
